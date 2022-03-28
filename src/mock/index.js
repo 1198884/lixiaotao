4c2b1697bffa9d0d293843a171d61a384b059ea3
LAPTOP-7TACJ4CD\1 <1198884968@qq.com>
2022-03-28 10:10:37 +0800

    first commit

---
+++ src/mock/index.js
@@ -0,0 +1,7 @@
+
+require('./appearance/mock')
+require('./ecy/mock')
+require('./shouye/mock')
+require('./qingzhan/heping/mock')
+require('./qingzhan/chuanyue/mock')
+require("./index1/index1.js")
